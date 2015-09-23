f2b7a52b71cbd8a8cf466550a048e4196950cd10
Florian Brinkmann <mail@flobri.de>
2015-09-23 12:46:24 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+	<main role="main">
+		<?php if ( have_posts() ) {
+			/* Start the Loop */
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'content', get_post_format() );
+			}
+		}
+		the_posts_pagination( array( 'type' => 'list' ) ); ?>
+	</main>
+<?php get_sidebar();
+get_footer();
