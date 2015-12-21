eaa8ea9833f91a75a43877a195e9e62f0445cf8c
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-12-21 12:21:48 +0100

    v0.15

---
+++ inc/class-featured-galleries.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Featured_Galleries widget class
+ */
+class Bornholm_Featured_Galleries extends WP_Widget {
+
+	public function __construct() {
+		$widget_ops = array(
+			'classname'   => 'widget_featured_galleries',
+			'description' => _x( 'Enter ids of galleries you want to feature.', 'Description of the featured galleries widget', 'bornholm' )
+		);
+		parent::__construct( 'featured-galleries', _x( 'Featured Galleries', 'Name of the featured galleries widget', 'bornholm' ), $widget_ops );
+	}
+
+	public function widget( $args, $instance ) {
+
+		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
+
+		$gallery_ids = ! empty( $instance['gallery_ids'] ) ? explode( ',', $instance['gallery_ids'] ) : array();
+		$counter     = 0;
+		foreach ( $gallery_ids as $gallery_id ) {
+			$post   = get_post( $gallery_id );
+			$format = get_post_format( $gallery_id );
+			if ( $post && $format == 'gallery' ) {
+				if ( $counter == 0 ) {
+					echo $args['before_widget'];
+					if ( $title ) {
+						echo $args['before_title'] . $title . $args['after_title'];
+					}
+				}
+				$images = bornholm_get_gallery_images( $gallery_id ); ?>
+				<div>
+					<?php bornholm_gallery_header( 'h4', $images, 'thumbnail', $post ); ?>
+				</div>
+				<?php $counter ++;
+			}
+		}
+		if ( $counter != 0 ) {
+			echo $args['after_widget'];
+		}
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$instance                = $old_instance;
+		$instance['title']       = strip_tags( $new_instance['title'] );
+		$instance['gallery_ids'] = strip_tags( $new_instance['gallery_ids'] );
+
+		return $instance;
+	}
+
+	public function form( $instance ) {
+		$title       = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+		$gallery_ids = isset( $instance['gallery_ids'] ) ? esc_attr( $instance['gallery_ids'] ) : '';
+		?>
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'bornholm' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>"
+			       name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>"/>
+		</p>
+
+		<p><label
+				for="<?php echo $this->get_field_id( 'gallery_ids' ); ?>"><?php _e( 'Comma seperated ids of galleries you want to feature:', 'bornholm' ); ?></label>
+			<input class="widefat" placeholder="523, 547" id="<?php echo $this->get_field_id( 'gallery_ids' ); ?>"
+			       name="<?php echo $this->get_field_name( 'gallery_ids' ); ?>" type="text"
+			       value="<?php echo $gallery_ids; ?>"/></p>
+
+		<?php
+	}
+}
---
+++ inc/class-recent-galleries.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Recent_Galleries widget class
+ */
+class Bornholm_Recent_Galleries extends WP_Widget {
+
+	public function __construct() {
+		$widget_ops = array(
+			'classname'   => 'widget_recent_galleries',
+			'description' => _x( 'Your site’s most recent galleries.', 'Description of the recent galleries widget', 'bornholm' )
+		);
+		parent::__construct( 'recent-galleries', _x( 'Recent Galleries', 'Name of the recent galleries widget', 'bornholm' ), $widget_ops );
+	}
+
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
+
+		$number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 2;
+		if ( ! $number ) {
+			$number = 2;
+		}
+
+		/**
+		 * Filter the arguments for the Recent Galleries widget
+		 *
+		 * @see WP_Query::get_posts()
+		 *
+		 * @param array $args An array of arguments used to retrieve the recent posts.
+		 */
+		$result = new WP_Query( apply_filters( 'widget_galleries_args', array(
+			'posts_per_page'      => $number,
+			'no_found_rows'       => true,
+			'post_status'         => 'publish',
+			'ignore_sticky_posts' => true,
+			'tax_query'           => array(
+				array(
+					'taxonomy' => 'post_format',
+					'field'    => 'slug',
+					'terms'    => 'post-format-gallery'
+				)
+			)
+		) ) );
+
+		if ( $result->have_posts() ) {
+			echo $args['before_widget'];
+			if ( $title ) {
+				echo $args['before_title'] . $title . $args['after_title'];
+			}
+			while ( $result->have_posts() ) {
+				$result->the_post();
+				$post   = get_post( get_the_ID() );
+				$images = bornholm_get_gallery_images( $post->ID );
+				if ( $images ) { ?>
+					<div>
+						<?php bornholm_gallery_header( 'h4', $images, 'thumbnail', $post ); ?>
+					</div>
+				<?php }
+			}
+			echo $args['after_widget'];
+// Reset the global $the_post as this query will have stomped on it
+			wp_reset_postdata();
+		}
+	}
+
+
+	public function update( $new_instance, $old_instance ) {
+		$instance           = $old_instance;
+		$instance['title']  = strip_tags( $new_instance['title'] );
+		$instance['number'] = (int) $new_instance['number'];
+
+		return $instance;
+	}
+
+	public function form( $instance ) {
+		$title  = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+		$number = isset( $instance['number'] ) ? absint( $instance['number'] ) : 2;
+		?>
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'bornholm' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>"
+			       name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>"/>
+		</p>
+
+		<p><label
+				for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of galleries to show:', 'bornholm' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'number' ); ?>"
+			       name="<?php echo $this->get_field_name( 'number' ); ?>" type="number"
+			       value="<?php echo $number; ?>"/></p>
+
+		<?php
+	}
+}
