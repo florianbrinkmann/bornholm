4a50c32be17a2462b5dbf69a9a4216d0bf172419
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-04-25 12:09:56 +0200

    Removed scripts directory

--- scripts/deploy-staging.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-ssh -p22 $STAGING_SERVER_USER@$STAGING_SERVER "mkdir -p $STAGING_PATH_STABLE"
-ssh -p22 $STAGING_SERVER_USER@$STAGING_SERVER "mkdir -p $STAGING_PATH_TRUNK"
-rsync -rav -e ssh --exclude='.git/' --exclude=scripts/ --exclude='.travis.yml' --delete-excluded ./ $STAGING_SERVER_USER@$STAGING_SERVER:$STAGING_PATH_TRUNK
-rsync -rav -e ssh --exclude='.git/' --exclude=scripts/ --exclude='.travis.yml' --delete-excluded ./ $STAGING_SERVER_USER@$STAGING_SERVER:$STAGING_PATH_STABLE
